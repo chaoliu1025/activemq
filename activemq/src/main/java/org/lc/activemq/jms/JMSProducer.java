08be603a62d403784b5fc021eb3e0bad98008b76
chaoliu1025 <chaoliu1025@163.com>
2017-05-24 16:14:12 +0800

    add activemq test project

---
+++ activemq/src/main/java/org/lc/activemq/jms/JMSProducer.java
@@ -0,0 +1,49 @@
+package org.lc.activemq.jms;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+
+public class JMSProducer {
+    /**
+     * 由于是测试代码，这里忽略了异常处理。
+     * 正是代码可不能这样做
+     * @param args
+     * @throws RuntimeException
+     */
+    public static void main (String[] args) throws Exception {
+        // 定义JMS-ActiveMQ连接信息（默认为Openwire协议）
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        Session session = null;
+        Destination sendQueue;
+        Connection connection = null;
+
+        //进行连接
+        connection = connectionFactory.createQueueConnection();
+        connection.start();
+
+        //建立会话（设置一个带有事务特性的会话）
+        session = connection.createSession(true, Session.SESSION_TRANSACTED);
+        //建立queue（当然如果有了就不会重复建立）
+        sendQueue = session.createQueue("/test");
+        //建立消息发送者对象
+        MessageProducer sender = session.createProducer(sendQueue);
+        TextMessage outMessage = session.createTextMessage();
+        outMessage.setText("这是发送的消息内容");
+
+        //发送（JMS是支持事务的）
+        sender.send(outMessage);
+        session.commit();
+
+        //关闭
+        sender.close();
+        connection.close();
+    }
+
+}
